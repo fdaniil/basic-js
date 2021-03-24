cde619368bf7fb76f2dc5e163ae37f4a2fc8c711
fdaniil <frolov.dan.mail@gmail.com>
2021-03-25 02:27:33 +0300

    feat: finish simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+
+  x: "",
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.x.split("~~").length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(!arguments.length)
+      value = "";
+    if (this.x != "")
+      this.x += "~~";
+    this.x += `( ${value} )`;
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof position != "number" || ![...this.x.split("~~").keys()].includes(position)) {
+      this.x = "";
+      throw Error;
+    }
+    this.x = this.x.split("~~").filter((e, k) => k != position - 1).join("~~");
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.x = this.x.split("~~").reverse().join("~~");
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let res = this.x;
+    this.x = "";
+    return res;
   }
 };
 
