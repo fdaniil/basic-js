e9106e615eee085d74cd8f2a400fe7d91cea147e
fdaniil <frolov.dan.mail@gmail.com>
2021-03-30 01:14:28 +0300

    feat: finish repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let res = "";
+  let separator = 'separator' in options ? options.separator : '+',
+  additionSeparator = 'additionSeparator' in options ? options.additionSeparator : '|',
+  repeatTimes = 'repeatTimes' in options ? options.repeatTimes : 1,
+  additionRepeatTimes = 'additionRepeatTimes' in options ? options.additionRepeatTimes : 1,
+  addition = 'addition' in options ? options.addition : "";
+  //.repeat(5).slice(0,-1)
+
+  return (str+((addition+additionSeparator).repeat(additionRepeatTimes).slice(0,-additionSeparator.length))+separator).repeat(repeatTimes).slice(0,-separator.length);
 };
   
